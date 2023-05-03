97b7e0e1fea2d9ebcc16f2fffafadf986c4ebc4d
DESKTOP-SMTK8E3\Etherquartz <progquartz@gmail.com>
2023-05-04 01:42:46 +0900

    ThermalReaction #2 - 물이 들어간 냄비 구현 완료 - 물이 끓고 연기가 나오는 등의 기능 구현 완료

---
+++ Assets/Scripts/Cooking/CookingTool/GasBurnerSwitch.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasBurnerSwitch : MonoBehaviour
+{
+
+    public float switchInput = 0.0f;
+
+    public float GetSwitchValue()
+    {
+        return switchInput;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Cooking/CookingTool/WaterPot.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPot : MonoBehaviour
+{
+    [SerializeField]
+    private Material waterBubbleMat;
+    [SerializeField]
+    private AudioSource waterBubbleSound;
+    [SerializeField]
+    private ThermalObject waterTemperature;
+    [SerializeField]
+    private GameObject waterSteam;
+
+    private float bubbleMatMin = 0.0f;
+    private float bubbleMatMax = 0.6f;
+
+    private float soundMin = 0.0f;
+    private float soundMax = 0.4f;
+
+    private bool isSoundPlayed = false;
+
+
+    private void Awake()
+    {
+        waterBubbleMat = transform.GetChild(1).GetComponent<MeshRenderer>().materials[0];
+        waterBubbleSound = transform.GetChild(4).GetComponent<AudioSource>();
+        waterSteam = transform.GetChild(3).gameObject;
+        waterTemperature = GetComponent<ThermalObject>();
+    }
+
+    private void Update()
+    {
+        if (waterTemperature.temperature > 80)
+        {
+            if(!waterSteam.activeInHierarchy)
+            {
+                waterSteam.SetActive(true);
+            }
+            if(!isSoundPlayed)
+            {
+                waterBubbleSound.Play();
+                isSoundPlayed = true;
+            }
+
+            float waterRaio = ((waterTemperature.temperature - 80.0f )/ 20.0f);
+            Debug.Log(waterRaio);
+
+            waterBubbleMat.color = new Color(1, 1, 1, waterRaio * bubbleMatMax);
+            waterBubbleSound.volume = soundMax * waterRaio;
+        }
+        else
+        {
+            if (waterSteam.activeInHierarchy)
+            {
+                waterSteam.SetActive(false);
+            }
+            waterBubbleMat.color = new Color(1,1,1,bubbleMatMin);
+            waterBubbleSound.volume = soundMin;
+        }
+
+
+    }
+}
