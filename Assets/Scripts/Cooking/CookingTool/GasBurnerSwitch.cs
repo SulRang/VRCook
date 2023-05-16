4382d025afdcde2ca31a66891384157ddb0f4759
DESKTOP-SMTK8E3\Etherquartz <progquartz@gmail.com>
2023-05-17 01:09:27 +0900

    가스 불 및 물 떨어지는 부분 수정. - 가스 불 부분이 눌릴 경우, 해당 오브젝트가 딸깍거리면서 불이 켜지게 됨.

--- Assets/Scripts/Cooking/CookingTool/GasBurnerSwitch.cs
+++ Assets/Scripts/Cooking/CookingTool/GasBurnerSwitch.cs
@@ -4,8 +4,69 @@ using UnityEngine;
 
 public class GasBurnerSwitch : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip sparkAudio;
+    [SerializeField]
+    private AudioClip fireComeOutAudio;
+    [SerializeField]
+    private AudioClip fireOnAudio;
 
+    private AudioSource audioSouce;
+
+    public bool isPressed = false;
+    public bool fireOn = false;
     public float switchInput = 0.0f;
+
+    private void Start()
+    {
+        audioSouce = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        switchInput = (transform.rotation.eulerAngles.z - 270) / 360;
+        if (transform.rotation.eulerAngles.z > 360)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - 360f));
+        }
+        else if (transform.rotation.eulerAngles.z < -360)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + 360f));
+        }
+
+        if (isPressed && switchInput > 0.1f && switchInput < 0.9f)
+        {
+            fireOn = true;
+            isPressed = false;
+        }
+        else if(!isPressed && switchInput < 0.1f && switchInput < 0.9f)
+        {
+            fireOn = false;
+        }
+        
+        if(isPressed && !fireOn)
+        {
+            if (audioSouce.clip != sparkAudio)
+            {
+                audioSouce.clip = sparkAudio;
+                audioSouce.Play();
+            }
+        }
+        else if(fireOn)
+        {
+            if(audioSouce.clip != fireOnAudio)
+            {
+                audioSouce.clip = fireOnAudio;
+                audioSouce.Play();
+            }
+        }
+        else if(!isPressed && !fireOn)
+        {
+            audioSouce.clip = null;
+            audioSouce.Pause();
+        }
+    }
+
+
 
     public float GetSwitchValue()
     {
---
+++ Assets/Scripts/TestCheck/CheckPotatoSaladCook.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPotatoSaladCook : MonoBehaviour
+{
+    public bool isWashedHand = false;
+
+}
