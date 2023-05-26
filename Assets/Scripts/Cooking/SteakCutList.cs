537c2486c49e2ee094077601c7d69baf0c2fa9f6
BaekMinSu <angelace1007@gmail.com>
2023-05-26 23:53:37 +0900

    스테이크 자르기 수정

---
+++ Assets/Scripts/Cooking/SteakCutList.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteakCutList : MonoBehaviour
+{
+    private static SteakCutList _instance;
+    // 인스턴스에 접근하기 위한 프로퍼티
+    public static SteakCutList Instance
+    {
+        get
+        {
+            // 인스턴스가 없는 경우에 접근하려 하면 인스턴스를 할당해준다.
+            if (!_instance)
+            {
+                _instance = FindObjectOfType(typeof(SteakCutList)) as SteakCutList;
+
+                if (_instance == null)
+                    Debug.Log("no Singleton obj");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        // 인스턴스가 존재하는 경우 새로생기는 인스턴스를 삭제한다.
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public Material[] mats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
